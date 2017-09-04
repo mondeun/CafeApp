e6463ebdefbe39fbefbd9b7be5f5ded9b008944a
Kenneth Glawe <kenneth.glawe@gmail.com>
2017-09-04 11:59:21 +0200

    Add FAKE

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+if test "$OS" = "Windows_NT"
+then # Windows
+  .paket/paket.exe
+  exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+    exit $exit_code
+  fi
+  dotnet restore
+  exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+    exit $exit_code
+  fi
+  packages/FAKE/tools/FAKE.exe $@ --fsiargs build.fsx
+else # Non Windows
+  mono .paket/paket.exe
+  exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+    exit $exit_code
+  fi
+  dotnet restore
+  exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+    exit $exit_code
+  fi
+  mono packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
+fi
