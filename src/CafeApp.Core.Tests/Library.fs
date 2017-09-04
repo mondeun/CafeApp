6731f5ad74174b1745ad5bd478c75a9a12f40992
Kenneth Glawe <kenneth.glawe@gmail.com>
2017-09-04 12:07:16 +0200

    Put projects in src

---
+++ src/CafeApp.Core.Tests/Library.fs
@@ -0,0 +1,5 @@
+namespace CafeApp.Core.Tests
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
---
+++ src/CafeApp.Core/Library.fs
@@ -0,0 +1,5 @@
+namespace CafeApp.Core
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
