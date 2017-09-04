03805af61e0044b711bd16abc23f4709e941d004
Kenneth Glawe <kenneth.glawe@gmail.com>
2017-09-04 13:42:10 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,12 +13,12 @@ then # Windows
   fi
   packages/FAKE/tools/FAKE.exe $@ --fsiargs build.fsx
 else # Non Windows
-  mono .paket/paket.exe
+  mono .paket/paket.exe install
   exit_code=$?
   if [ $exit_code -ne 0 ]; then
     exit $exit_code
   fi
-  dotnet restore
+  mono dotnet restore
   exit_code=$?
   if [ $exit_code -ne 0 ]; then
     exit $exit_code
